887f973338ea4025987fd43f8c9cfb982de9d6d2
lau <laura.nino.rosas@gmail.com>
2026-08-09 17:30:42 -0500

    Update ejecutar_linux.sh

--- ejecutar_linux.sh
+++ ejecutar_linux.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 rm grafica.png
 rm resultados.csv
-echo "Lenguaje ; Tiempo ; Memoria" > resultados.csv
+echo "Lenguaje,Tiempo,Memoria" > resultados.csv
 
-/usr/bin/time -f "Python ; %e ; %M" python3 experimento_python.py 2>>resultados.csv
+/usr/bin/time -f "Python,%e,%M" python3 experimento_python.py 2>>resultados.csv
 
 gcc experimento_c.c -o experimento_comp
-/usr/bin/time -f "C ; %e ; %M" ./experimento_comp 2>>resultados.csv
-rm experimento_comp 
+/usr/bin/time -f "C,%e,%M" ./experimento_comp 2>>resultados.csv
+rm experimento_comp
 
 python3 graficar.py
